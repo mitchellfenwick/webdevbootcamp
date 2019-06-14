16da7fcc365fff37622590ca9611bf9a8166451e
Mitchell Fenwick <mitchell.fenwick@gmail.com>
2019-06-14 17:48:18 +1200

    Work on Comments
    Added ability to create new comments.
    Restructured view files

--- YelpCamp/v4/app.js
+++ YelpCamp/v4/app.js
@@ -3,6 +3,7 @@ var express    = require("express"),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment    = require("./models/comment");
     seedDB     = require("./seeds");
 
 
@@ -41,7 +42,7 @@ app.get("/campgrounds", function(req, res) {
        if (err) {
            console.log(err);
        } else {
-           res.render("index", {campgrounds:allCampgrounds});
+           res.render("campgrounds/index", {campgrounds:allCampgrounds});
        }
     });
     //res.render("campgrounds", {campgrounds: campgrounds});
@@ -50,7 +51,7 @@ app.get("/campgrounds", function(req, res) {
 
 // New campground
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new"); 
+    res.render("campgrounds/new"); 
 });
 
 // SHOW - shows more info about one campground
@@ -60,7 +61,7 @@ app.get("/campgrounds/:id", function(req, res){
        if (err){
            console.log(err);
        } else {
-            res.render("show", {campground:foundCampground}); 
+            res.render("campgrounds/show", {campground:foundCampground}); 
        }
     });
     //render show template with that campground
@@ -83,6 +84,45 @@ app.post("/campgrounds", function(req, res){
    });
 
    //redirect back to campgrounds page
+});
+
+//==============================================
+// COMMENTS ROUTES
+//==============================================
+
+// New Route
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+// Create Route
+app.post("/campgrounds/:id/comments", function(req, res){
+    //Lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            // create new comment
+            Comment.create(req.body.comment, function(err, comment){
+                if (err) {
+                    console.log(err);
+                } else {
+                    // connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect campground show page
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    })    
 });
 
 app.listen(8080, "127.0.0.1", function(){
