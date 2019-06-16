1505d148b6c617311f4ba2268773742ecc2b040d
Mitchell Fenwick <mitchell.fenwick@gmail.com>
2019-06-16 13:53:19 +1200

    Created v9

---
+++ YelpCamp/v9/routes/comments.js
@@ -0,0 +1,59 @@
+//==============================================
+// Comments Routes
+//==============================================
+
+var express = require("express");
+var router = express.Router({mergeParams: true});
+var Campground = require("../models/campground"),
+    Comment = require("../models/comment");
+
+// New Route
+router.get("/new", isLoggedIn, function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+// Create Route
+router.post("/", isLoggedIn, function(req, res){
+    //Lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            // create new comment
+            Comment.create(req.body.comment, function(err, comment){
+                if (err) {
+                    console.log(err);
+                } else {
+                    // add username and id to comment
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    // Save the comment
+                    comment.save();
+                    // connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect campground show page
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    })    
+});
+
+ // Middleware
+ // Checks if a user is logged in
+ function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
