0fa22584d201827262c2465e267a715801a44715
Mitchell Fenwick <mitchell.fenwick@gmail.com>
2019-06-16 20:07:29 +1200

    Added update route to comments

--- YelpCamp/v9/routes/comments.js
+++ YelpCamp/v9/routes/comments.js
@@ -47,6 +47,28 @@ router.post("/", isLoggedIn, function(req, res){
     })    
 });
 
+// Edit route
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if (err) {
+            res.redirect("/campgrounds/" + req.params.id);
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    })
+});
+
+// Update route
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if (err) {
+            res.redirect("/campgrounds/"+ req.params.id + "/comments/" + req.params.comment_id + "/edit");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
  // Middleware
  // Checks if a user is logged in
  function isLoggedIn(req, res, next){
