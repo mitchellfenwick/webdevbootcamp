2c6ec53ef268421cebf8f7b50200eb3a6fb99d2d
Mitchell Fenwick <mitchell.fenwick@gmail.com>
2019-06-16 13:20:01 +1200

    Added campground functionality
    Made it so that in order to create a campgorund you have to be logged in and the campgrounds are now associated with a user.

--- YelpCamp/v8/routes/campgrounds.js
+++ YelpCamp/v8/routes/campgrounds.js
@@ -45,12 +45,18 @@ router.post("/", isLoggedIn, function(req, res){
    var name = req.body.name
    var image = req.body.image
    var description = req.body.description;
-   var newCampground = {name: name, image: image, description: description};
+   var author = {
+       id: req.user._id,
+       username: req.user.username
+   }
+   var newCampground = {name: name, image: image, description: description, author: author};
    // Create a new campground and save to DB
    Campground.create(newCampground, function(err, newlyCreated){
       if (err) {
           console.log(err);
-      } else {res.redirect("/campgrounds");
+      } else {
+          console.log(newlyCreated)
+          res.redirect("/campgrounds");
       }
    });
 
